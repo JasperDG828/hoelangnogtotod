0c0f41de264a926e6d6d704489abc5a1721c6cf4
Jasper De Gussemé <contact.jdgbe@yahoo.com>
2022-05-28 19:58:36 +0200

    undid a feature cuz doesnt work

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const timeLeftElement = document.getElementById("timeLeft");
+var now = moment();
 var od = moment("2022-07-03T14:00");
-var endOd = moment("2022-07-09T10:00");
 
 function loadFile(filePath) {
   var result = null;
@@ -15,31 +15,26 @@ function loadFile(filePath) {
 
 function refreshTime() {
   var now = moment();
-  if (!now.isBetween(od, endOd)) {
-    var days = od.diff(now, "days");
-    var hours = od.diff(now, "hours") - days * 24;
-    var minutes = od.diff(now, "minutes") - od.diff(now, "hours") * 60;
-    var seconds =
-      od.diff(now, "seconds") - 60 * minutes - 3600 * hours - 86400 * days;
-    timeLeftElement.innerHTML =
-      days +
-      "dagen " +
-      String(hours).padStart(2, "0") +
-      "u " +
-      String(minutes).padStart(2, "0") +
-      "min " +
-      String(seconds).padStart(2, "0") +
-      "sec ";
-  }
-  timeLeftElement.innerHTML = "Het is bezig!!!";
+  var days = od.diff(now, "days");
+  var hours = od.diff(now, "hours") - days * 24;
+  var minutes = od.diff(now, "minutes") - od.diff(now, "hours") * 60;
+  var seconds =
+    od.diff(now, "seconds") - 60 * minutes - 3600 * hours - 86400 * days;
+  timeLeftElement.innerHTML =
+    days +
+    "dagen " +
+    String(hours).padStart(2, "0") +
+    "u " +
+    String(minutes).padStart(2, "0") +
+    "min " +
+    String(seconds).padStart(2, "0") +
+    "sec ";
 }
 
 const urls = JSON.parse(loadFile("./urls.json"));
 const body = document.getElementById("body");
-
 function newBackground() {
   var url = urls[Math.floor(Math.random() * urls.length)];
-
   fetch(url)
     .then((response) => response.blob())
     .then((imageBlob) => {
